71108e9a264e1fd154366f7b65206da20fd02ec6
Norhajar Gabuya <hajarguiamatgabuya@gmail.com>
2024-06-08 13:53:22 +0800

    adjustment to database

--- knexfile.js
+++ knexfile.js
@@ -1,8 +1,7 @@
-// Update with your config settings.
-
 /**
  * @type { Object.<string, import("knex").Knex.Config> }
  */
+
 module.exports = {
   development: {
     client: 'mysql',
@@ -10,40 +9,51 @@ module.exports = {
       host: 'localhost',
       user: 'root',
       password: '',
-      database: 'am9commercial'
-    }
+      database: 'am9commercial',
+      connectionLimit: 20,
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+    },
   },
 
   staging: {
-    client: 'postgresql',
+    client: 'mysql',
     connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+      host: 'b18rvltkeprxyrwdxj35-mysql.services.clever-cloud.com',
+      user: 'u1llarmjzoo3t683',
+      password: 'HKDuDLkfJO1cZQJX58M7',
+      database: 'b18rvltkeprxyrwdxj35',
+      connectionLimit: 20,
     },
     pool: {
       min: 2,
-      max: 10
+      max: 10,
     },
     migrations: {
-      tableName: 'knex_migrations'
-    }
+      tableName: 'knex_migrations',
+    },
   },
 
   production: {
-    client: 'postgresql',
+    client: 'mysql',
     connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+      host: 'b18rvltkeprxyrwdxj35-mysql.services.clever-cloud.com',
+      user: 'u1llarmjzoo3t683',
+      password: 'HKDuDLkfJO1cZQJX58M7',
+      database: 'b18rvltkeprxyrwdxj35',
+      connectionLimit: 20,
     },
     pool: {
       min: 2,
-      max: 10
+      max: 10,
     },
     migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
-
+      tableName: 'knex_migrations',
+    },
+  },
 };
